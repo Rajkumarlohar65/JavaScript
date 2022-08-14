195f03a2369b816064b3ed2cf65123d937fa6ddf
Rajkumarlohar65 <78910492+Rajkumarlohar65@users.noreply.github.com>
2022-08-14 09:16:47 +0530

    adding files

---
+++ 02_variables_and_datatypes.js
@@ -0,0 +1,55 @@
+//*** 1. Ways to print in JavaScript ***//
+// console.log("Hello World!");
+// alert("Welcome to JavaScript Tutorial");
+// document.write("this is my document.write");
+
+// *** 2. JavaScript Console Api *** //
+// console.log("Hello World!", 6 + 4, "Another log");
+// console.warn("this is warning");
+// console.error("this is the error");
+
+//*** 3. JavaScript Variables ***//
+var number1 = 10;
+var number2 = 10;
+// console.log("The sum of num1 and num2 is:", number1 + number2);
+
+//*** 4. Datatypes in JavaScript *** //
+// Numbers
+var num1 = 45;
+var num2 = 46;
+
+// String
+var str1 = "this is a string";
+var str2 = "this is also a string";
+
+// Booleans
+var a = true;
+var b = false;
+// console.log(a,b);
+
+// Undefined
+// var und = undefined;
+var und;
+// console.log(und);
+
+// Null
+var n = null;
+// console.log(n);
+
+/*
+At a very high level , there are two types of data types in JavaScript
+1. Primitive data types: undefined, null, number, string, symbol
+2. Reference data types: Objects and Arrays
+*/ 
+
+//objects
+var marks = {
+    ravi: 23,
+    shubham: 45,
+    ram: 99.99
+}
+// console.log(marks);
+
+// Arrays
+var arr = [1, 2, 3, "four", 5];
+// console.log(arr);
---
+++ 03_operators.js
@@ -0,0 +1,30 @@
+// Operators in JavaScript
+// Arithmatic Operators
+var a = 100;
+var b = 10;
+console.log("The value of a + b is: ", a + b);
+console.log("The value of a - b is: ", a - b);
+console.log("The value of a / b is: ", a / b);
+console.log("The value of a * b is: ", a * b);
+
+// Assignment Operators
+// var c = b;
+// c += 2;
+// c *= 2;  // c = c - 2
+// c /= 2;
+// console.log(c);
+
+// Comparison Operators
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a == b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// Logical Operators
+// console.log(true && true);
+// console.log(true && false);
+// console.log(true || true);
+// console.log(true || false);
+// console.log(!false) // not false
+// console.log(!true) // not true
